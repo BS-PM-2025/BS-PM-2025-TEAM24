e2f8ea109ef0c8028100297c883c2b2f3f0837c6
Mohamad040 <mohamda8@ac.sce.ac.il>
2025-04-28 19:15:30 +0300

    BSPM25T24-3   BSPM25T24-2   BSPM25T24-1   Login commit

--- my-react-app/src/App.jsx
+++ my-react-app/src/App.jsx
@@ -4,6 +4,8 @@ import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-d
 import CustomerSignUp from './screens/CustomerSignUp.jsx';
 import WorkerSignup from './screens/WorkerSignup.jsx'; 
 import ForgotPassword from './screens/ForgotPassword.jsx';
+import Login from './screens/Login.jsx';
+
 
 
 
@@ -11,7 +13,8 @@ function App() {
   return (
     <Router>
       <Routes>
-        
+        <Route path="/" element={<Navigate to="/Login" replace />} /> 
+        <Route path="/login" element={<Login />} />
         <Route path="/CustomerSignUp" element={<CustomerSignUp />} />
         <Route path="/WorkerSignup" element={<WorkerSignup />} />
         <Route path="/ForgotPassword" element={<ForgotPassword />} />
