bbf70c3d740251d0b8fa5fa996c5100754602982
sajedrizkalla1 <sajedab@ac.sce.ac.il>
2025-06-11 02:43:08 +0300

    BSPM25T24-159  customer final accept

---
+++ server/routers/workRateRouter.js
@@ -0,0 +1,11 @@
+const { Router } = require('express');
+const { verifyToken } = require('../middlewares/authJwt');
+const wr = require('../controllers/workRateController');
+
+const workRateRouter = new Router();
+
+/* anyone logged-in can rate & read */
+workRateRouter.get('/:workerId',        verifyToken, wr.getWorkerRatings);
+workRateRouter.get('/avg/:workerId',    verifyToken, wr.getWorkerAverage);
+
+module.exports = { workRateRouter };
