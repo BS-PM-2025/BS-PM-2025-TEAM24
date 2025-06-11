4e551ff517d572955573034b94aa1f490a1a25a0
basheza <basheza@ac.sce.ac.il>
2025-06-11 17:01:20 +0300

    events router and controller update

--- server/routers/eventsRouter.js
+++ server/routers/eventsRouter.js
@@ -1,19 +1,23 @@
-const { Router } = require('express');
+const { Router }          = require('express');
 const { eventsController } = require('../controllers/eventsController');
+const { verifyToken }      = require('../middlewares/authJwt');
+
 const eventsRouter = new Router();
 
-// Assuming you have these methods in your eventsController
-eventsRouter.get('/getEvents', eventsController.getEvents);
-eventsRouter.get('/getEventsByType/:callType', eventsController.getEventsByType);
-eventsRouter.post('/addEvent', eventsController.addEvent); // Note: Adjusted route for adding an event
-eventsRouter.delete('/deleteEvent/:id', eventsController.deleteEvent);
-eventsRouter.put('/updateEvent/:callID', eventsController.updateEvent);
-eventsRouter.post('/getLocationDetails', eventsController.getLocationDetails);
+eventsRouter.get('/getEvents',                    verifyToken, eventsController.getEvents);
+eventsRouter.get('/getEventsByType/:callType',    verifyToken, eventsController.getEventsByType);
+eventsRouter.post('/addEvent',                    verifyToken, eventsController.addEvent);
+eventsRouter.delete('/deleteEvent/:id',           verifyToken, eventsController.deleteEvent);
 eventsRouter.delete('/applicants/:id',  verifyToken,  eventsController.unapply);
+eventsRouter.put('/updateEvent/:callID',          verifyToken, eventsController.updateEvent);
+eventsRouter.post('/getLocationDetails',          verifyToken, eventsController.getLocationDetails);
+eventsRouter.get('/myCalls', verifyToken, eventsController.getMyEvents);
+eventsRouter.get('/applicants/:id', verifyToken, eventsController.getApplicants);
+eventsRouter.post('/approve/:id/:workerId',       verifyToken, eventsController.approveWorker);
+eventsRouter.post('/applicants/:id',              verifyToken, eventsController.applyToCall);
 eventsRouter.get('/myApplications',               verifyToken,eventsController.getMyApplications);
 eventsRouter.get('/getApprovedCalls',  verifyToken, eventsController.getMyApprovedCalls);
 eventsRouter.put('/update/:id',          verifyToken, eventsController.cusupdateEvent);
 eventsRouter.post('/completeCall/:id', verifyToken, eventsController.completeCall);
-eventsRouter.post('/approve/:id/:workerId',       verifyToken, eventsController.approveWorker);
-eventsRouter.post('/applicants/:id',              verifyToken, eventsController.applyToCall);
+eventsRouter.put('/rated/:id', verifyToken, eventsController.markRated);
 module.exports = { eventsRouter };
